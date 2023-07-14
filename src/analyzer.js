d47d0e67650ee898ec658aa7a539aca8ee223e0d
ginapedraza <ginapedraza00@gmail.com>
2023-07-14 11:56:20 -0400

    mi primer commit

--- src/analyzer.js
+++ src/analyzer.js
@@ -19,4 +19,5 @@ const analyzer = {
   },
 };
 
+
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer` 
+
+let resetButton = document.getElementById('reset-button');
+
+    resetButton.addEventListener('click', function () {
+    document.querySelector('textarea[name="user-input"]').value = '';
+    });
+
