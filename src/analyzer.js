554fc683c8bf09a63fa787093eb9488974a22fbf
ginapedraza <ginapedraza00@gmail.com>
2023-07-17 12:15:23 -0400

    primeras funciones

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,15 +1,23 @@
 const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getWordCount: (text) => { 
+    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    let words = text.trim().split(/\s+/).length;
+    return words;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    let characters = text.length;
+    return characters;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let charactersEx = text.replace(/[\s.,\/#!$¡%\^&\*;:{}=\-+_`~()¿?']/g, '').length;
+    return charactersEx;
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -2,9 +2,27 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer` 
 
-let resetButton = document.getElementById('reset-button');
 
-    resetButton.addEventListener('click', function () {
-    document.querySelector('textarea[name="user-input"]').value = '';
+// EVENTO KEYUP
+
+let textarea = document.querySelector('textarea[name="user-input"]');
+textarea.addEventListener('keyup', ()=>{
+    
+    let wordCountEl = document.querySelector('.word-count-el');
+    wordCountEl.innerHTML = analyzer.getWordCount(textarea.value) // argumento
+
+    let charactCount = document.querySelector('.charact-count-el');
+    charactCount.innerHTML = analyzer.getCharacterCount(textarea.value)
+
+    let charactCountExcludingSpaces = document.querySelector('.charact-count-no-spaces-el');
+    charactCountExcludingSpaces.innerHTML = analyzer.getCharacterCountExcludingSpaces(textarea.value)
+        
     });
 
+
+//BOTÓN PARA RESETEAR LA CAJA DE TEXTO AL HACER CLICK
+
+let resetButton = document.getElementById('reset-button');
+resetButton.addEventListener('click', function () {
+    textarea.value = '';
+    });
